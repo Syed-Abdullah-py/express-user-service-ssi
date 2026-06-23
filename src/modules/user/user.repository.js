f257bf6c913e9c2832b85d4061549f70649a740e
Syed Abdullah <smabdullah4945@gmail.com>
2026-06-24 00:17:25 +0500

    feat(user): Added missing comments

--- src/modules/user/user.repository.js
+++ src/modules/user/user.repository.js
@@ -1,9 +1,11 @@
 import { create } from './user.model';
 
+// REPOSITORY FOR CREATING USERS
 const createUser = async (data) => {
   return await create(data);
 };
 
+// REPOSITORY FOR READING USERS
 const getUsers = async () => {
   return await User.findAll();
 };
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,9 +1,11 @@
 import { createUser as _createUser } from './user.repository';
 
+// CREATE USER SERVICE
 const createUser = (data) => {
   return _createUser(data);
 };
 
+// READ USER SERVICE
 const getUsers = () => {
   return repo.getUsers();
 };
